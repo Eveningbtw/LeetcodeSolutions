1657353f1a734fe2500eb1a3d7024d8099aa4966
Eveningbtw <dumpleton.ashley@gmail.com>
2025-05-01 20:28:01 +0100

    Time: 3 ms (28.18%) | Memory: 17.8 MB (48.53%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -9,10 +9,11 @@ class Solution:
             # Iterate over each individual string in the array of strings
             for string in strings:
 
-                # If 
+                # If the index of the string is equal to the len of the string
                 if index == len(string) or string[index] != strings[0][index]:
                     return result
             
+            # Add the character at position @index of the first string in the array @strings
             result += strings[0][index]
         
         return result
