e823ecd656caf8d7a1e36551549c87fed74774ca
Eveningbtw <dumpleton.ashley@gmail.com>
2025-05-12 20:42:43 +0100

    Time: 1767 ms (22.14%) | Memory: 18.4 MB (87.33%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-
-        for pointer in range(0,len(nums)):
-            for i in range(pointer+1,len(nums)):
-                if nums[i]+nums[pointer] == target:
-                    return(pointer,i)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return(i,j)
