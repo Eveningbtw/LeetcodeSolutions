f194148de31ce58fe326503eae65267caae91fe1
Eveningbtw <dumpleton.ashley@gmail.com>
2025-07-07 15:25:56 +0100

    Time: 8 ms (5.07%) | Memory: 17.9 MB (48.22%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,9 +3,9 @@ class Solution:
         
         result = ""
 
-        # We are iterrating over every index of the array strs
         for i in (range(len(strs[0]))):
             for string in strs:
+                print(strs[0][i])
                 if i == len(string) or string[i] != strs[0][i]:
                     return result
             result += strs[0][i]
