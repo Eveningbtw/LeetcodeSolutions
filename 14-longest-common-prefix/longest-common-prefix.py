ca7233f39fc7f70b1691c171d4985dcf62063b30
Eveningbtw <dumpleton.ashley@gmail.com>
2025-07-07 15:18:19 +0100

    Time: 4 ms (11.14%) | Memory: 18 MB (30.45%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,19 +1,12 @@
 class Solution:
-    def longestCommonPrefix(self, strings: List[str]) -> str:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         
         result = ""
-        
-        # Iterate over the number of characters of the first word in the array of strings
-        for index in range(len(strings[0])):
 
-            # Iterate over each individual string in the array of strings
-            for string in strings:
-
-                # If the current index is equal to the length of a string (out of bounds) or the current index of the current string is not equal to the same index of the first string, then return result (return since the characters are not the same)
-                if index == len(string) or string[index] != strings[0][index]:
+        # We are iterrating over every index of the array strs
+        for i in (range(len(strs[0]))):
+            for string in strs:
+                if i == len(string) or string[i] != strs[0][i]:
                     return result
-            
-            # Add the character at position @index of the first string in the array @strings
-            result += strings[0][index]
-        
+            result += strs[0][i]
         return result
