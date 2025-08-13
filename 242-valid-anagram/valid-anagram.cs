a95fbe218d2846b6957c8e932e38a2f0292f2133
Eveningbtw <dumpleton.ashley@gmail.com>
2025-08-13 15:41:41 +0100

    Time: 60 ms (5.90%) | Memory: 46.5 MB (6.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,10 +1,9 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
 
-    if (s.Length != t.Length){
-        return false;
-    }
-
-    return String.Concat(s.OrderBy(c => c)) == String.Concat(t.OrderBy(c => c));
+        if (s.Length != t.Length){
+            return false;
+        }
+        return String.Concat(s.OrderBy(c => c)) == String.Concat(t.OrderBy(c => c));
     }
 }
