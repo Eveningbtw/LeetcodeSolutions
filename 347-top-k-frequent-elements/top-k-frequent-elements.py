b3a1ecc8deb8fb9bbbc711ef2fafdf22961eb707
Eveningbtw <dumpleton.ashley@gmail.com>
2025-08-31 23:36:27 +0100

    Time: 3 ms (90.53%) | Memory: 21.2 MB (80.97%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        hashMap = {}
+
+        for num in nums:
+            if num in hashMap:
+                hashMap[num] += 1
+            else:
+                hashMap[num] = 1
+        
+        sorted_items = sorted(hashMap.items(), key=lambda x: x[1], reverse=True)
+        return [k for k, v in sorted_items[:k]]
+
